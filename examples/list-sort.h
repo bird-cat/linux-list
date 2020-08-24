efaa2f6ea8cbb2b6a6645d24755247dd867e2089
tupolev4 <tupolev4@gmail.com>
2020-08-24 17:28:54 +0800

    Add some comments in list-sort.h

--- examples/list-sort.h
+++ examples/list-sort.h
@@ -1,6 +1,10 @@
 #ifndef LIST_SORT_H
 #define LIST_SORT_H
 
+/**
+ * list_XXXsort - sort a list by XXX sorting function
+ * @head: pointer to the head of the list
+ */
 void list_insertsort(struct list_head *head);
 void list_mergesort(struct list_head *head);
 void list_qsort(struct list_head *head);
